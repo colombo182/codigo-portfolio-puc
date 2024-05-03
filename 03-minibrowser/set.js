94c831fe5bb254c0609f2b814330d53a39822a1b
Pedro Polidori Colombo <colombo182@pop-os.tail346a7.ts.net>
2024-05-03 15:26:00 -0300

    first commit

---
+++ 03-minibrowser/set.js
@@ -0,0 +1,12 @@
+// TDD - Test Driven Development (procurar)
+var novoSet = new Set();
+
+novoSet.add('backend');
+novoSet.add('frontend');
+novoSet.add('mobile');
+novoSet.add('mobile');
+novoSet.add('mobile');
+novoSet.add('mobile');
+
+var resultado = novoSet;
+console.log(resultado);
