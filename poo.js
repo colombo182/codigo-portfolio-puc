f6146ae464e0354e1db9cfb234a0b200309e2d6b
PEDRO POLIDORI COLOMBO <pedro_colombo@hotmail.com>
2024-05-13 03:05:58 +0000

    Fim parte 1 aula POO

--- poo.js
+++ poo.js
@@ -11,3 +11,28 @@
 // Ruby
 
 // Objetos são uma estrutura um pouco mais complexa que as funções
+
+// Programação estruturada - Paradigma com foco em:
+// Sequência - instrução executada uma depois da outra
+// Decisão - instrução executada após algum teste lógico
+// Iteração - trecho de código pode repetir-se após algum teste lógico
+
+// Programação Procedural
+// Paradigma com foco no uso de procedimentos e funções para facilitar o reuso
+// f1() - x; f2() -y;
+
+// Programação Estruturada e Procedural
+// Código espaguete 
+// Muito "copia e cola" de funções
+// Mudanças em uma função resultam em mudanças em outras funções
+// Interdependência entre as funções
+
+let numero1 = 10;
+let numero2 = 20;
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+let z = add(numero1, numero2);
+console.log(z);
