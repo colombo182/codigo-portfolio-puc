2b5b8f74dd95d8f3c4890ab327c2e679ba5094f0
PEDRO POLIDORI COLOMBO <pedro_colombo@hotmail.com>
2024-05-13 13:49:10 +0000

    Fim da Parte 2 da Aula 1 de POO

--- poo.js
+++ poo.js
@@ -27,6 +27,8 @@
 // Mudanças em uma função resultam em mudanças em outras funções
 // Interdependência entre as funções
 
+
+//Procedural
 let numero1 = 10;
 let numero2 = 20;
 
@@ -36,3 +38,59 @@ function add(n1, n2) {
 
 let z = add(numero1, numero2);
 console.log(z);
+
+// POO - Paradigma com foco no uso de objetos, onde cada um contém suas variáveis e funções
+// Objeto A - Método (função) f1() e atributo (propriedade) x
+// Objeto B - Método (função) f2() e atributo (propriedade) y
+
+// Componentes de um Objeto:
+// Nome (ex. Dispositivo)
+// Atributos (ex. fabricante, modelo, memória)
+// Métodos (ex. ligar(), diminuirVolume(), tirarFoto())
+
+// Objeto:
+// coleção de dados e/ou funcionalidades com alguma relação entre si
+// dados -> variáveis (atributos ou propriedades)
+// funcionalidades -> funções (métodos)
+// EM Javascript, atributos e métodos são MEMBROS de um objeto, cada um com um nome e um valor
+
+var nomeDoObjeto = {
+    nomeMembro1: valorMembro1,
+    nomeMembros2: valorMembros2,
+    nomeMembros3: valorMembros3
+};
+
+var pessoa = {
+    nome: 'Joaquim',
+    idade: 20,
+    sexo: 'M',
+    saudar: function () {
+        console.log('Ola');
+    }
+};
+
+// Atributo - acesso duas formas:
+// objeto.propriedade
+// objeto ['propriedade']
+
+// Atribuilção - duas formas:
+// objeto.propriedade = valor
+// objeto ['propriedade'] = valor
+
+// Método - acesso duas formas:
+// objeto.metodo()
+// objeto.metodo(parametro)
+// objeto.metodo(parametro1, parametro2, ...)
+
+// Em Javascript QUASE TUDO é objeto
+// Javascript possui dois tipos de dados: 
+// Tipos de Valor - representações de valores imutáveis:
+// Number
+// String
+// Boolean
+// Symbol
+// Undefined
+// Null
+
+// Tipos de Referência - representações de valores mutáveis e complexos, quando comparados com os Tipos de valor:
+// Object (Object, Function, Array)
