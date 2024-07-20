6cab2c169cfc8001f8fb24f05e0ed61148d1a8d1
Pedro Polidori Colombo <colombo182@pop-os.tail346a7.ts.net>
2024-07-20 17:10:53 -0300

    End server-side aulas

--- server-side/server-crud-express/api-rest-express/routes/users.js
+++ server-side/server-crud-express/api-rest-express/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users lista. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Lista Todos Usuários');
+});
+
+/* GET users lista. */
+router.get('/:nome', function(req, res, next) {
+  const nome = req.params.nome;
+  res.send(`Lista Usuário de nome = ${nome}`);
+});
+
+/* POST user .*/
+router.post('/', (req, res) => {
+  res.send('Cria um Usuário')
+});
+
+/* DELETE User. */
+router.delete('/',(req,res) => {
+  res.send('Deleta um usuário')
+});
+
+/* PUT User altera*/
+router.put('/', (req,res) => {
+  res.send('Altera Usuario')
 });
 
 module.exports = router;
